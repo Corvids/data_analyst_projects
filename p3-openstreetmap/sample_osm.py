8073afcc0073d62a7426d181146eb2e9db0c15ab
Corvids <joonnifer@gmail.com>
2016-08-23 14:39:26 -0500

    in prog

--- p3-openstreetmap/sample_osm.py
+++ p3-openstreetmap/sample_osm.py
@@ -3,7 +3,7 @@
 
 import xml.etree.cElementTree as ET  # Use cElementTree or lxml if too slow
 
-OSM_FILE = "portlandOregon.osm"  # Replace this with your osm file
+OSM_FILE = "portland.osm"  # Replace this with your osm file
 SAMPLE_FILE = "sample.osm"
 
 # Parameter: take every k-th top level element
